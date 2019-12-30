38d47bc3059d9ea16d9a9f631fd0e8e5284e5836
jacky <mellouljacky@hotmail.com>
2019-12-30 18:55:34 +0100

    jaks1

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,8 +4,8 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		final int index;
-		for(i=0; i>=0; i++) {
-			System.out.println("Hello bugs !"):
+		for(i=0; i<5; i++) {
+			System.out.println("Hello bugs jacks !"):
 		}
 	}
 }
